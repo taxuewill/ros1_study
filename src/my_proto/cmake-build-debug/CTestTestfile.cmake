c8568233eb50962f69c1368db013d3777f508997
willwangjing <willwangjing@didiglobal.com>
2022-06-15 14:34:49 +0800

    add ignore

--- src/my_proto/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/will/data/github/ros1_study/src/my_proto
-# Build directory: /home/will/data/github/ros1_study/src/my_proto/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
