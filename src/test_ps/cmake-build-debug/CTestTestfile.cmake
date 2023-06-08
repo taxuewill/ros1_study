99f6060a2e6305238ed879dbd9e0c70ad452aec5
willwangjing <willwangjing@didiglobal.com>
2023-06-08 10:32:16 +0800

    append

--- src/test_ps/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/will/data/github/ros1_study/src/test_ps
-# Build directory: /home/will/data/github/ros1_study/src/test_ps/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
